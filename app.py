6d37dc70ce15976562313ee2e1eae953cdd6a6c5
zenkz <zhe_86@mail.ru>
2020-02-10 23:24:58 -0600

    Add a template

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
+#def index():
+#        return 'Hello world'
 
-def index():
-        return 'Hello world'
+@app.route('/')
+def hello_name():
+    user = "testpy"
+    return render_template('./index.html', name = user)
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
